33987212aa1423d6f8e73c26e0fda5dcde5ec5e6
liamoehlman <liam.oehlman@sidelab.com>
2011-03-03 16:24:14 +1000

    added the option for and/or

--- src/geoserver.js
+++ src/geoserver.js
@@ -1,79 +1,104 @@
 GEOSERVER = (function(){
+	
+	function bbox(args) {
+		args = COG.extend({
+			property: 'TheGeom',
+			min: '-90 -180',
+			max: '90 180'
+		}, args);
+
+		var min = T5.Geo.Position.parse(args.min),
+			max = T5.Geo.Position.parse(args.max);
+
+		return COG.formatStr('BBOX({0}, {1}, {2}, {3}, {4})',
+			args.property,
+			min.lat,
+			min.lon,
+			max.lat,
+			max.lon);
+	} // bbox
+	
+	function contains(args) {
+		args = COG.extend({
+			property: 'TheGeom',
+			type: 'POINT'
+		}, args);
+		
+	//	var cqlQuery = "CONTAINS("+args.property+", "+args.type+"("+pointCalc(args.coords)+"))";
+		return COG.formatStr('CONTAINS({0}, {1}({2}))',
+			args.property,
+			args.type,
+			pointCalc(args.coords));
+	} // contains
+	
+	function distance(args) {
+		args = COG.extend({
+			property: 'TheGeom',
+			type: 'POINT',
+			distance: '.05',
+			unit: 'kilometers'
+		}, args);
+		
+	//	var cqlQuery = "DWITHIN("+args.property+", "+args.type+"("+pointCalc(args.coords)+"), "+args.distance+", kilometers)";	
+		return COG.formatStr('DWITHIN({0}, {1}({2}), {3}, {4})',
+			args.property,
+			args.type,
+			pointCalc(args.coords),
+			args.distance,
+			args.unit);
+	} // distance
+	
+	function like(args) {
+		args = COG.extend({
+			property: 'TheGeom'
+		}, args);
+		
+	//	var cqlQuery = args.property + " LIKE '%25" + args.value + "%25'"; 
+		return COG.formatStr("{0} LIKE '%{1}%'",
+			args.property,
+			args.value);
+	} // like
+	
+	function cql(args) {
+		args = COG.extend({
+			operator: 'AND',
+			conditions: []
+		}, args);
+		
+		var queryElems = [];
+		
+		for (var ii = 0; ii < args.conditions.length; ii++) {
+			var condition = args.conditions[ii],
+				handler = null;
+			
+			if (condition.type) {
+				handler = queryHandlers[condition.type];
+			} // if
+			
+			// if we have a hander then execute it
+			if (handler) {
+				queryElems.push(handler(condition.args));
+			} // if
+		} // for
+		
+		return '(' + queryElems.join(' ' + args.operator + ' ') + ')';
+	} // cql
+	
 	/* internals */
 	var queryHandlers = {
-		'spatial.bbox' : function(args) {
-			var min = T5.Geo.Position.parse(args.min),
-				max = T5.Geo.Position.parse(args.max),
-				cqlQuery = "BBOX("+args.property+", "+min.lat+", "+min.lon+", "+max.lat+", "+max.lon+")";
-			return cqlQuery;
-		}, // spatial.bbox
-		'spatial.contains' : function(args) {
-			var cqlQuery = "CONTAINS("+args.property+", "+args.type+"("+pointCalc(args.coords)+"))";
-			return cqlQuery;
-		},
-		'spatial.cross' : function(args) {
-			var cqlQuery = "CROSS("+args.property+", "+args.type+"("+pointCalc(args.coords)+"))";
-			return cqlQuery;
-		},
-		'spatial.intersect' : function(args) {
-			var coords = [].concat(args.coords),
-				cqlQuery = "INTERSECT("+args.property+", GEOMETRYCOLLECTION (POINT ("+coords[0]+"),POINT ("+coords[1]+"),LINESTRING ("+pointCalc(coords.splice(0,2))+")) )";
-			return cqlQuery;
-		},
-		'spatial.distance' : function(args) {
-		// note - distince is actually degrees, the distance parameter in the query doesn't work
-			var cqlQuery = "DWITHIN("+args.property+", "+args.type+"("+pointCalc(args.coords)+"), "+args.distance+", kilometers)";
-			return cqlQuery;
-		},
-		like  : function(args) {
-			var cqlQuery = args.property + " LIKE '%25" + args.value + "%25'"; 
-			return cqlQuery;
-		} // like
+		'spatial.bbox': bbox,
+		'spatial.contains' : contains,
+		'spatial.distance' : distance,
+		like  : like,
+		cql: cql
 	};
 	
 	// Adds commas to the linestring except the last point
 	function pointCalc(coords) {
-		var points = '';
-
-		for (var ii = 0; ii<coords.length; ii++) {
-
-			if (ii === (coords.length - 1)) {
-				points = points + coords[ii];
-			} else {
-				points = points + coords[ii] + ",";
-			} // if...else
-		} // for
-		return points;
+		return coords.join(",");
 	} // pointCalc
 	
 	/* exports */
-	
-	function cql(json) {
-		var query = "";
-		var data = JSON.parse(json);
-		
-		// Creates the query for one entry
-		if (data.length === 1) {
-						
-			if (queryHandlers[data[0].type]) {
-				query = query+queryHandlers[data[0].type](data[0].args);
-			} // if
-		// Creates the query for multiple entries
-		} else {
-			
-			for (var ii=0 ; ii < data.length; ii++) {
-				
-				if (queryHandlers[data[ii].type] && ii<1) {
-					query = query+queryHandlers[data[ii].type](data[ii].args);
-				} // if
-				
-				if (queryHandlers[data[ii].type] && ii>0) {
-					query = query+ " AND " +queryHandlers[data[ii].type](data[ii].args);
-				} // if 
-			} // for					
-		} // if...else
-		return query;
-	} // cql
 
 	function buildRequest(params, cqlJson, callback) {
 		// initialise defaults
@@ -81,7 +106,7 @@ GEOSERVER = (function(){
 			service: 'WFS',
 			version: '1.1.0',
 			request: 'GetFeature',
-			maxFeatures: 200,
+			maxFeatures: 20000,
 			outputFormat: 'json'
 		}, params);
 		
